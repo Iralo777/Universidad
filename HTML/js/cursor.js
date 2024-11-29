03e6cf079b1c10117eac55663e77e8bd32ac949c
Iralo777 <iratxe.angulotx@gmail.com>
2024-11-29 16:54:26 +0100

    con el cursor añadido

--- HTML/js/cursor.js
+++ HTML/js/cursor.js
@@ -1,4 +1,21 @@
 //■■■■■■■■■■■■■■■■■■■■■■■■■■■■ cursor ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+    //Cursor Background Tracking
+    const tokenBgCursor = document.querySelector('.blob-cursor__bg');
+    document.addEventListener('mousemove', e => {
+        tokenBgCursor.setAttribute("style", "top: " + (e.pageY - 100) + "px; left: " + (e.pageX - 100) + "px;")
+    });
+    const triggerCursor = document.querySelector('.show-cursor--bg');
+    const cursorBg = document.querySelector('.blob-cursor__bg');
+    triggerCursor.addEventListener('mouseenter', () => {
+        cursorBg.classList.remove('hide');
+    });
+    triggerCursor.addEventListener('mouseleave', () => {
+        cursorBg.classList.add('hide');
+    });
+
+
+//■■■■■■■■■■■■■■■■■■■■■■■■■■■■ sonido enlaces ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
 document.addEventListener("DOMContentLoaded", function() {
     // Cargar el sonido
     const sonidoHover = new Audio("sonido/magic.mp3");
